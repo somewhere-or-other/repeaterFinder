a5d7a1d9c335f25ecc236bdad7599211dfc2e4df
Lloyd Brown <somewhere.or.other@gmail.com>
2023-06-03 20:07:06 -0600

    beginning structure and notes for figuring out bearings in 3d; this is *NOT* working yet

--- repeaterFinder.py
+++ repeaterFinder.py
@@ -3,8 +3,12 @@
 import csv
 import io
 import requests
+import pyproj
+
 import pprint
 
+
+geodesic = pyproj.Geod(ellps='WGS84')
 
 def processRepeaterList(url="https://utahvhfs.org/rptrraw.txt"):
     r = requests.get(url)
@@ -18,7 +22,10 @@ def processRepeater(repeaterRow=None):
     if repeaterRow==None:
         raise ValueError("No repeater definition found")
     
-    pprint.pprint(repeaterRow)
+    #pprint.pprint(repeaterRow)
+
+def getBearing(lat1, long1, elev1, lat2, long2, elev2):
+    #TODO: figure out how to us pyproj to convert using epsg 4979 (3d), and NOT EPSG 4236 (2d); see https://gis.stackexchange.com/a/385738
 
 if __name__=="__main__":
     processRepeaterList()
